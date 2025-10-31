d0b0b0661cd85d38a2b5983449cc25beaba8dd84
Noah Harman <noahharman190@gmail.com>
2025-10-31 09:19:29 -0400

    oct31

---
+++ blinking_a_led.v
@@ -0,0 +1,15 @@
+module blinking_a_led (
+    input i_clk,
+    input i_switch_1,
+    output o_LED_1
+);
+
+reg r_LED_1    = 1'b0;
+reg r_switch_1 = 1'b0;
+
+always @(posedge i_clk) begin 
+    r_switch_1 <= i_switch_1; 
+    if(i_switch_1 == 1'b0 && r_switch_1 == 1'b1) r_LED_1 <= ~r_LED_1;
+end 
+
+assign o_LED_1 = r_LED_1;
